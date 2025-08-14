bf17a9e3c7e350a7494e0f62323c3dab665a65cd
StupidGuy1122 <e1374568@u.nus.edu>
2025-08-14 19:35:15 +0800

    Pytest

--- app/recommender.py
+++ app/recommender.py
@@ -73,8 +73,7 @@ def recommend_activities(user_id, top_k):
     recommended['score'] = top_scores.cpu().numpy()
 
     recommended = recommended.rename(columns={'activityid': 'id'})
-    return "not a list"
-    #recommended[['id', 'title', 'score']]
+    return recommended[['id', 'title', 'score']]
 
 
 @torch.no_grad()
